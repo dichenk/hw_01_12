8c81e606ff270c1569035fa237797f81dad6a67d
dichenk <dchenk@gmail.com>
2022-12-04 11:23:35 +0300

    working

--- hw_01_12/functions.py
+++ hw_01_12/functions.py
@@ -1,27 +1,69 @@
 import vaex
+import json
+import hashlib
 
 file_path = "stocks_archive_5years.zip"
-file_output = "dump.csv"
 sort_columns = "high"
 limit = 30
 group_by_name = False
 order = 'AAL'
-filename='dump.csv'
 
 date_sec = "2017-08-08"
-name_sec="PCLN"
-filename_sec='dump_sec.csv'
+name_sec = "PCLN"
+filename = 'dump'
+filename_type = '.csv'
 
+## декоратор для функций, который считает количество вызовов функции (для соответствующего имени файла)
+def decor_count(func):
+    def wrapper(*args, **kwargs):
+        wrapper.counter += 1
+        return func(*args, **kwargs)
+    wrapper.counter = 0
+    return wrapper
 
+## функция кэширования запроса
+def cash_function(cashed_arguements, amount_of_calling):
+    try:
+        with open('cash.json', 'r') as my_favourite_json:
+            data = json.load(my_favourite_json)
+    except:
+        data = {}
+    try: 
+        ## ищем запрос в кэшэ запросов
+        return data[cashed_arguements]
+    except: 
+        ## записываем запрос в кэш запросов
+        data[cashed_arguements] = amount_of_calling
+        with open('cash.json', 'w') as my_favourite_json:
+            json.dump(data, my_favourite_json)
+        return 0
 
-def select_sorted(sort_columns_f = sort_columns, limit_f = limit, group_by_name_f = group_by_name, order_f = order, filename_f = filename):
-    dv = vaex.from_csv(file_path, convert=True)
-    dvv = dv[dv.Name == order_f]
-    dvv = dvv.sort(sort_columns_f, ascending=False)
-    dvv[0:limit_f].export_csv(filename_f)
+## Функция выполнения сортировки и фильтра
+def select_sorted_backdoor(sort_columns, limit, order):
+    dv = vaex.from_csv(file_path, convert = True)
+    dv = dv[dv.Name == order]
+    dv = dv.sort(sort_columns, ascending = False)
+    dv  = dv[0:limit]
+    return dv
 
-def get_by_date(date = date_sec, name=name_sec, filename=filename_sec):
+@decor_count
+def select_sorted(sort_columns_f = sort_columns , limit_f = limit, group_by_name_f = group_by_name, order_f = order):
+    a = ['select_sorted_backdoor', sort_columns_f, str(limit_f), str(group_by_name_f), order_f]
+    a = ','.join(a)
+    cash_fu = cash_function(a, select_sorted.counter)
+    if cash_fu == 0:
+        ## Вызов функции впервые. Вызываем, обрабатываем, кэшируем результат
+        b = select_sorted_backdoor(sort_columns_f, limit_f, order_f)
+        b.export_hdf5(str(select_sorted.counter) + '.hdf5')
+        b.export_csv(filename + str(select_sorted.counter) + filename_type)
+    else:
+        ## Функцию не вызываем, достаем инфу из кэша
+        b = vaex.open(str(cash_fu) + '.hdf5')
+        b.export_csv(filename + str(select_sorted.counter) + filename_type)
+
+def get_by_date(date = date_sec, name=name_sec):
+    ## одна из функций домашней работы
     dv = vaex.from_csv(file_path, convert=True)
     dvv = dv[dv.Name == name]
     dvv = dvv[dv.date == date]
-    dvv.export_csv(filename)
+    dvv.export_csv(filename + filename_type)
